dca464b1a9d4af15462df951c388b35a534845c4
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2024-03-22 15:32:21 +0900

    feat - 바텀 네비게이션 이동

--- presentation/src/main/java/com/knot/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/knot/presentation/ui/main/MainActivity.kt
@@ -3,15 +3,16 @@ package com.knot.presentation.ui.main
 import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.knot.presentation.PageState
 import com.knot.presentation.R
 import com.knot.presentation.base.BaseActivity
 import com.knot.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding, PageState.Default, MainActivityViewModel>(
-    ActivityMainBinding::inflate) {
+class MainActivity :
+    BaseActivity<ActivityMainBinding, MainActivityPageState, MainActivityViewModel>(
+        ActivityMainBinding::inflate
+    ) {
 
     override val viewModel: MainActivityViewModel by viewModels()
     private lateinit var navController: NavController
@@ -26,12 +27,50 @@ class MainActivity : BaseActivity<ActivityMainBinding, PageState.Default, MainAc
 
     override fun initState() {
         super.initState()
-
-
+        repeatOnStarted {
+            viewModel.eventFlow.collect {
+                inspectEvent(it as MainEvent)
+            }
+        }
     }
 
     private fun initNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            changeBottomNavigationView(destination.id)
+        }
+    }
+
+    private fun inspectEvent(event: MainEvent) {
+        when (event) {
+            MainEvent.GoToCreateKnot -> {}
+            MainEvent.GoToKnotList -> {
+                navController.navigate(R.id.knotListFragment)
+            }
+
+            MainEvent.GoToMain -> {
+                navController.navigate(R.id.mainFragment)
+            }
+
+            MainEvent.GoToProfile -> {
+                navController.navigate(R.id.profileFragment)
+            }
+
+            MainEvent.GoToSetting -> {
+                navController.navigate(R.id.settingFragment)
+            }
+        }
+    }
+
+    private fun changeBottomNavigationView(id: Int) {
+        when (id) {
+            R.id.mainFragment -> viewModel.activeMain()
+            R.id.knotListFragment -> viewModel.activeKnotList()
+            R.id.profileFragment -> viewModel.activeProfile()
+            R.id.settingFragment -> viewModel.activieSetting()
+            else -> viewModel.goneNavigation()
+        }
     }
 }
--- presentation/src/main/java/com/knot/presentation/ui/main/MainActivityViewModel.kt
+++ presentation/src/main/java/com/knot/presentation/ui/main/MainActivityViewModel.kt
@@ -1,13 +1,99 @@
 package com.knot.presentation.ui.main
 
-import com.knot.presentation.PageState
+import androidx.lifecycle.viewModelScope
 import com.knot.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainActivityViewModel @Inject constructor() : BaseViewModel<PageState.Default>() {
-    override val uiState: PageState.Default
-        get() = TODO("Not yet implemented")
+class MainActivityViewModel @Inject constructor() : BaseViewModel<MainActivityPageState>() {
 
+    private val isMainPageStateFlow : MutableStateFlow<Boolean> = MutableStateFlow(true)
+    private val isKnotListPageStateFlow : MutableStateFlow<Boolean> = MutableStateFlow(false)
+    private val isProfilePageStateFlow : MutableStateFlow<Boolean> = MutableStateFlow(false)
+    private val isSettingPageStateFlow : MutableStateFlow<Boolean> = MutableStateFlow(false)
+    private val isOtherPageStateFlow : MutableStateFlow<Boolean> = MutableStateFlow(false)
+
+    override val uiState: MainActivityPageState = MainActivityPageState(
+        isMainPageStateFlow.asStateFlow(),
+        isKnotListPageStateFlow.asStateFlow(),
+        isProfilePageStateFlow.asStateFlow(),
+        isSettingPageStateFlow.asStateFlow(),
+        isOtherPageStateFlow.asStateFlow()
+    )
+
+    fun onClickMain() {
+        activeMain()
+        emitEventFlow(MainEvent.GoToMain)
+    }
+
+    fun onClickKnotList() {
+        activeKnotList()
+        emitEventFlow(MainEvent.GoToKnotList)
+    }
+
+    fun onClickCreateKnot() {
+        goneNavigation()
+        emitEventFlow(MainEvent.GoToCreateKnot)
+    }
+
+    fun onClickProfile() {
+        activeProfile()
+        emitEventFlow(MainEvent.GoToProfile)
+    }
+
+    fun onClickSetting() {
+        activieSetting()
+        emitEventFlow(MainEvent.GoToSetting)
+    }
+
+    fun activeMain(){
+        viewModelScope.launch {
+            isMainPageStateFlow.update { true }
+            isKnotListPageStateFlow.update { false }
+            isProfilePageStateFlow.update { false }
+            isSettingPageStateFlow.update { false }
+            isOtherPageStateFlow.update { false }
+        }
+    }
+
+    fun activeKnotList(){
+        viewModelScope.launch {
+            isMainPageStateFlow.update { false }
+            isKnotListPageStateFlow.update { true }
+            isProfilePageStateFlow.update { false }
+            isSettingPageStateFlow.update { false }
+            isOtherPageStateFlow.update { false }
+        }
+    }
+
+    fun activeProfile(){
+        viewModelScope.launch {
+            isMainPageStateFlow.update { false }
+            isKnotListPageStateFlow.update { false }
+            isProfilePageStateFlow.update { true }
+            isSettingPageStateFlow.update { false }
+            isOtherPageStateFlow.update { false }
+        }
+    }
+
+    fun activieSetting(){
+        viewModelScope.launch {
+            isMainPageStateFlow.update { false }
+            isKnotListPageStateFlow.update { false }
+            isProfilePageStateFlow.update { false }
+            isSettingPageStateFlow.update { true }
+            isOtherPageStateFlow.update { false }
+        }
+    }
+
+    fun goneNavigation(){
+        viewModelScope.launch {
+            isOtherPageStateFlow.update { true }
+        }
+    }
 }
